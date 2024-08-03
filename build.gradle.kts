911db2d9e525c11a240d24c375e7dfda1c96bc63
seunghwan <seosh817@naver.com>
2024-08-03 13:29:14 +0900

    build: network 통신을 위한 라이브러리 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinJvm) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.secrets) apply false
 }

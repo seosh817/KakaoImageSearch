4d4c8a5795733943ed479ce734e3b11b7a3adef8
seunghwan <seosh817@naver.com>
2024-08-05 00:58:28 +0900

    temp

--- app/src/main/java/com/seosh817/kakaoimagesearch/ui/MainUiState.kt
+++ app/src/main/java/com/seosh817/kakaoimagesearch/ui/MainUiState.kt
@@ -1,6 +1,9 @@
 package com.seosh817.kakaoimagesearch.ui
 
+import com.seosh817.kakaoimagesearch.domain.entity.AppSettings
+import com.seosh817.kakaoimagesearch.domain.entity.DarkThemeMode
+
 sealed interface MainUiState {
-    data object Success : MainUiState
+    data class Success(val appSettings: AppSettings) : MainUiState
     data object Loading : MainUiState
 }
---
+++ core/domain/src/main/java/com/seosh817/kakaoimagesearch/domain/entity/OpenDialog.kt
@@ -0,0 +1,7 @@
+package com.seosh817.kakaoimagesearch.domain.entity
+
+enum class OpenDialog {
+    NONE,
+    APP_THEME_SETTINGS,
+    APP_LANGUAGE_SETTINGS
+}

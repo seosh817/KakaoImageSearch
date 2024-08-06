8f1de97f00c3d580d9ed6dfb6308c72bb8cf877f
seunghwan <seosh817@naver.com>
2024-08-06 10:19:33 +0900

    fix: Localization 버그 수정

--- app/src/main/java/com/seosh817/kakaoimagesearch/util/LocalizationUtil.kt
+++ app/src/main/java/com/seosh817/kakaoimagesearch/util/LocalizationUtil.kt
@@ -8,22 +8,23 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.LocaleListCompat
 import com.seosh817.kakaoimagesearch.domain.entity.AppLanguage
 
-object LanguageUtil {
+object LocalizationUtil {
 
     fun setLanguage(context: Context, appLanguage: AppLanguage) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
             if (appLanguage == AppLanguage.DEFAULT) {
                 context.getSystemService(LocaleManager::class.java)
-                    .applicationLocales = LocaleList.getDefault()
+                    .applicationLocales = LocaleList.forLanguageTags("xx-YY")
+
             } else {
                 context.getSystemService(LocaleManager::class.java)
                     .applicationLocales = LocaleList.forLanguageTags(appLanguage.locale)
             }
         else {
             if (appLanguage == AppLanguage.DEFAULT)
-                AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags(appLanguage.locale))
+                AppCompatDelegate.setApplicationLocales(LocaleListCompat.getEmptyLocaleList())
             else
-                AppCompatDelegate.setApplicationLocales(LocaleListCompat.getDefault())
+                AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags(appLanguage.locale))
         }
     }
 }
